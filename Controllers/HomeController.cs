3b3f9517f7ec2eff85eb028c03da7b6d0352d406
KAMATCHIAMMAN <113223223+KAMATCHIAMMAN@users.noreply.github.com>
2023-06-22 09:43:57 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using HotelBookingMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Text;
+
+namespace HotelBookingMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        Uri baseAddress = new Uri("https://localhost:7114/api");
+        private readonly HttpClient _client;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _client = new HttpClient();
+            _client.BaseAddress = baseAddress;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(Register userRegistration)
+        {
+            try
+            {
+                string data = JsonConvert.SerializeObject(userRegistration);
+                StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = _client.PostAsync(_client.BaseAddress + "/Authentication/AddUser", content).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    //TempData["successMessage"] = "User Registered successfully";
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["errorMessage"] = ex.Message;
+                return View();
+            }
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return RedirectToAction("Index");
+        }
+
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
